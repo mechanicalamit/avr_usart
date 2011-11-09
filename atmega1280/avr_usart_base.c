9355063bbe86d7a1e3db95dae59738f64915f60d
Amit Dhingra <mechanicalamit@gmail.com>
2011-11-08 20:00:13 -0600

    atmega1280/avr_usart_base.c : Clean slate
    Signed-off-by: Amit Dhingra <mechanicalamit@gmail.com>

--- atmega1280/avr_usart_base.c
+++ atmega1280/avr_usart_base.c
@@ -30,49 +30,10 @@ int main (void)
 
 	UCSR0B |= (1 << RXCIE0); // Enable the USART Recieve Complete interrupt (USART_RXC)
 	
-	/* Timer 0 will count and overflow at every 1 ms
-	 * F_CPU = 16 MHz, prescalar = 62, timer0 = 250kHz
-	 * 1 ms = 250 ticks */
-	TCNT0 = 0; /* Init timer */
-	TCCR0A = _BV(WGM01); /* CTC mode */
-	OCR0A = 250; /* 250 ticks */
-	TIMSK0 = _BV(OCIE0A); /* Enable overflow interrupt */
-	TCCR0B = _BV(CS01) | _BV(CS00); /* prescalar = 64 */
-
-
-	/* PCF PWM setup */
-	TCCR1B = _BV(CS10); /* No prescaling */
-	TCCR1B |= _BV(WGM13);
-/*	TCCR1A = _BV(WGM10);  PWM */
-
-	TCCR1A |= _BV(COM1B1); /* Non-inverting */
-	TCCR1A |= _BV(COM1A1); /* Non-inverting */
-	TCCR1A |= _BV(COM1C1); /* Non-inverting */
-
-	ICR1 = 0xFFFF; /* TOP = MAX */
-
-	TIMSK1 |= _BV(TOIE1);
-
-	OCR1B = 0x0c7F; /* output pin OC1B, PB6 */
-	OCR1A = 0x0c7F; 
-	OCR1C = 0x0c7F; 
-
-	DDRB = _BV(PB7) | _BV(PB6);
-
-	srand(TCNT1);
 
 	sei();
 	for (;;) // Loop forever
 	{
-		if (rand() == 0)
-			send_time_usart();
-		if (rand() % 10 == 0){
-			if (bit_is_set(PINB, PB0))
-				send_byte_usart0('N');
-			else
-				send_byte_usart0('F');
-		}
-
 	}   
 
 }
@@ -84,66 +45,3 @@ void send_byte_usart0(char ch)
 	UDR0 = ch;
 }
 
-void send_time_usart(void)
-{
-#define time_str_len 32
-	uint8_t i;
-	char time_str[time_str_len];
-
-	itoa((uint16_t)clock_minute, time_str, 10);
-	for (i=0; i<=time_str_len-1; ++i){
-		if ((time_str[i] == '\0') || (time_str[i] == '\n'))
-			break;
-		send_byte_usart0(time_str[i]);
-	}
-	send_byte_usart0(':');
-	itoa((uint16_t)clock_second, time_str, 10);
-	for (i=0; i<=time_str_len-1; ++i){
-		if ((time_str[i] == '\0') || (time_str[i] == '\n'))
-			break;
-		send_byte_usart0(time_str[i]);
-	}
-	send_byte_usart0(':');
-	itoa((uint16_t)clock_millisecond, time_str, 10);
-	for (i=0; i<=time_str_len-1; ++i){
-		if ((time_str[i] == '\0') || (time_str[i] == '\n'))
-			break;
-		send_byte_usart0(time_str[i]);
-	}
-	send_byte_usart0('\n');
-}
-
-ISR(TIMER1_OVF_vect)
-{
-	OCR1C = ((OCR1C>>6)+OCR1C+1) % 0XFFFF;
-	OCR1B = ((OCR1B>>6)+OCR1B+2) % 0XFFFF; 
-}
-
-ISR(USART0_RX_vect)
-{
-   char ReceivedByte;
-   ReceivedByte = UDR0; // Fetch the recieved byte value into the variable "ByteReceived"
-   if (ReceivedByte & 0x20)
-		   ReceivedByte &= 0xDF;
-   else
-		   ReceivedByte |= 0x20;
-   send_byte_usart0(ReceivedByte); // Echo back the received byte back to the computer
-} 
-//The output compate interrupt handler
-//We set up the timer in such a way that
-//this ISR is called exactly at 1ms interval
-ISR(TIMER0_COMPA_vect)
-{
-	clock_millisecond++;
-	if(clock_millisecond==1000) {
-		clock_second++;
-		clock_millisecond=0;
-		if(clock_second==60) {
-			clock_minute++;
-			clock_second=0;
-			}
-		if(clock_minute==60) {
-			clock_minute=0;
-		}
-	}
-}
